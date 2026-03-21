dd5b8e67599217de131b1d679cf4d00320cc0e00
zlaud <laudkimberly@gmail.com>
2026-03-21 12:36:43 -0700

    database setup

---
+++ backend/db/database.py
@@ -0,0 +1,10 @@
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+supabase: Client = create_client(
+    os.environ["SUPABASE_URL"],
+    os.environ["SUPABASE_KEY"]
+)
